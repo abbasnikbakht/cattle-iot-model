428381dfc7e2caf9a8eda2bf0ef79d83b0ea0991
Lijo Varghese <lijo.varghese@seqato.com>
2019-08-19 11:40:59 +0530

    added start scripts

---
+++ data_inject/starter_script.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+source /home/seqato-lijo/workspace/cattle-iot-model/catenv/bin/activate
+processes=( "python monitor.py" "python file_processor.py")
+
+start() {
+
+    echo virtualenv is now active.
+    echo running the scripts
+    for i in "${processes[@]}"
+        do
+           if pgrep -f "$i" &>/dev/null; then
+                echo "$i already running. please stop it first"
+           else
+                $i &
+        fi
+        done
+
+}
+
+stop() {
+
+    for i in "${processes[@]}"
+        do
+           if pgrep -f "$i" &>/dev/null; then
+                echo "Stopping "$i""
+                kill $(pgrep -f "$i")
+           else
+                echo "$i not running"
+
+
+        fi
+        done
+
+}
+
+case "$1" in
+    'start')
+            start
+            ;;
+    'stop')
+            stop
+            ;;
+    'restart')
+            stop ; echo "Sleeping..."; sleep 1 ;
+            start
+            ;;
+    *)
+            echo
+            echo "Usage: $0 { start | stop | restart | status }"
+            echo
+            exit 1
+            ;;
+esac
+
+exit 0
